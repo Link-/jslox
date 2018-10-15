ebf3cd28d4aa003c81b50afda5e232f18f0b6715
Bassem Dghaidi <bassem@interop.link>
2018-10-16 00:47:45 +0300

    Add reserved keywords

---
+++ src/Keywords.js
@@ -0,0 +1,17 @@
+module.exports = {
+  and:    'AND',
+  else:   'ELSE',
+  false:  'FALSE',
+  for:    'FOR',
+  fun:    'FUN',
+  if:     'IF',
+  nil:    'NIL',
+  or:     'OR',
+  print:  'PRINT',
+  return: 'RETURN',
+  super:  'SUPER',
+  this:   'THIS',
+  true:   'TRUE',
+  var:    'VAR',
+  while:  'WHILE'
+}

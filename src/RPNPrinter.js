f0011247d24e6f0926c5d6c5dc62ea226c72763c
Bassem Dghaidi <bassem@interop.link>
2018-10-24 00:41:27 +0300

    Refactor add docblock comments

--- src/RPNPrinter.js
+++ src/RPNPrinter.js
@@ -2,18 +2,34 @@ const Expr = require('./expressions/Expr.js');
 
 class RPNPrinter {
 
+  /**
+   * 
+   * @param {*} expr 
+   */
   print(expr) {
     return expr.accept(this);
   }
 
+  /**
+   * 
+   * @param {*} expr 
+   */
   visitBinaryExpr(expr) {
     return this.rpn(expr.operator.lexeme, expr.left, expr.right);
   }
 
+  /**
+   * 
+   * @param {*} expr 
+   */
   visitGroupingExpr(expr) {
     return this.rpn("", expr.expression);
   }
 
+  /**
+   * 
+   * @param {*} expr 
+   */
   visitLiteralExpr(expr) {
     if (expr.value == null) {
       return "nil";
@@ -22,10 +38,19 @@ class RPNPrinter {
     return expr.value.toString();
   }
 
+  /**
+   * 
+   * @param {*} expr 
+   */
   visitUnaryExpr(expr) {
     return this.rpn(expr.operator.lexeme, expr.right);
   }
 
+  /**
+   * 
+   * @param {*} name 
+   * @param  {...any} exprs 
+   */
   rpn(name, ...exprs) {
     let result = ``;
 
